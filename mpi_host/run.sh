d536f214b8b8b6d26e8860ab5054bc3db419aca9
KoalaYuFeng <yufeng@nus.edu.sg>
2022-12-27 21:23:09 +0800

    integrate mpi into xrt develop

--- mpi_host/run.sh
+++ mpi_host/run.sh
@@ -1,1 +1,2 @@
-/opt/openmpi/bin/mpirun -n 2 --hostfile host_file ./mpi_host
+/opt/openmpi/bin/mpirun -x LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/opt/xillinx/xrt/lib/ -n 2 --hostfile host_file ./mpi_host
+
