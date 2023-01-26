be3e68e9f092f93ba1f4c9bbdac167d7f870d0b5
KoalaYuFeng <yufeng@nus.edu.sg>
2023-01-26 21:59:09 +0800

    add graph off-line partition method

--- dataset/graph_partition.py
+++ dataset/graph_partition.py
@@ -3,7 +3,7 @@ import os
 
 dataset = [['R19', 'rmat-19-32.txt'], ['R21','rmat-21-32.txt'], ['R24','rmat-24-16.txt'], ['G23', 'graph500-scale23-ef16_adj.edges'],
            ['G24', 'graph500-scale24-ef16_adj.edges'], ['G25', 'graph500-scale25-ef16_adj.edges'], ['WP', 'wikipedia-20070206.mtx'], 
-           ['HW', 'ca-hollywood-2009.mtx'], ['LJ', 'LiveJournal1.txt'], ['TW', 'soc-twitter-2010.mtx']]
+           ['HW', 'ca-hollywood-2009.mtx'], ['LJ', 'LiveJournal1.txt'], ['TW', 'soc-twitter-2010.mtx'], ['R12', 'rmat-12-4.txt']]
 print (dataset)
 
 for dataset_index in range(len(dataset)):
@@ -24,6 +24,7 @@ for dataset_index in range(len(dataset)):
     ## filename = '/data/graph_dataset/rmat-12-4.txt'
     partition_size = 1024*1024 ## can be 1024*1024 or 512*1024
     sub_partition_num = 12
+    UNMAPPED_FLAG = 4294967295 ## 0xffffffff, max vertex index
     END_FLAG = 4294967294 ## 0xffffffff - 1, end_flag value
     alignment_size = 4*1024 ## edge num of each sub-partition should be 4K align
 
@@ -44,9 +45,20 @@ for dataset_index in range(len(dataset)):
     ## print(min_index_temp)
 
     outdegree = np.zeros(max_index + 1, dtype = np.int32)
-    outdegree[ : max_index_temp+1] = np.bincount(src_array)
+    outdegree[ : max_index_temp+1] = np.bincount(src_array) ## start from 0 to max+1
     np.savetxt(path + "/outDeg.txt", outdegree, fmt='%d', delimiter=' ')
     ## print(outdegree)
+
+    mapping_index = np.zeros(max_index + 1, dtype = np.uint32) ## compressed vertex index to original index
+    tmp_index = 0
+    for i in range(len(mapping_index)):
+        if (outdegree[i] == 0):
+            mapping_index[i] = UNMAPPED_FLAG ## 0xFFFFFFFF means invalid vertex
+        else:
+            mapping_index[i] = tmp_index
+            tmp_index += 1
+    np.savetxt(path + "/index_mapping.txt", mapping_index, fmt='%d', delimiter=' ')
+    print ("After compress, vertex number ", tmp_index + 1)
 
     delete_array = np.where(outdegree == 0)
     ## print(delete_array)
@@ -55,9 +67,16 @@ for dataset_index in range(len(dataset)):
     ## print(delete_index)
     txt_array = np.delete(txt_array, delete_index, axis = 0)
     ## print(txt_array)
-    print ("After  compress, edge number ", len(txt_array))
+    print ("After compress, edge number ", len(txt_array))
 
     ## partition function
+    for i in range(len(txt_array)):
+        if ((mapping_index[txt_array[i][0]] == UNMAPPED_FLAG) | (mapping_index[txt_array[i][1]] == UNMAPPED_FLAG)):
+            print("Wrong index in vertex compression")
+            exit(-1)
+        else:
+            txt_array[i][0] = mapping_index[txt_array[i][0]]
+            txt_array[i][1] = mapping_index[txt_array[i][1]]
 
     txt_array = txt_array[np.lexsort([txt_array.T[1]])] ## sort array by incremental order
     array_after_ption = [0]
@@ -76,7 +95,9 @@ for dataset_index in range(len(dataset)):
         ## print (txt_array[array_after_ption[i]:array_after_ption[i+1],:])
         ## print (txt_array[array_after_ption[i]:array_after_ption[i+1],:].shape)
         array_temp = txt_array[array_after_ption[i]:array_after_ption[i+1],:] ## each partition array
-        
+        ## sort partition array
+        array_temp = array_temp[np.lexsort([array_temp.T[0]])]
+
         edge_num_p = len(array_temp)
         edge_num_p = edge_num_p/sub_partition_num
         edge_num_p = np.int32((edge_num_p + alignment_size - 1)/alignment_size) * alignment_size
--- dataset/readme.txt
+++ dataset/readme.txt
@@ -2,4 +2,4 @@
 Eg: python graph_partition.py
 ## some notes: 
 1. need to make sure you had downloaded dataset to /data/graph_dataset directory.
-2. need to delete the notation of each dataset. (since our code can not process string words such as "#,%")
+2. need to delete the notation of original dataset. (since our code can not process string words such as "#,%")
